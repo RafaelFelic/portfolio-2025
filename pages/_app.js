b6566d002f96e3906a868417277382c788000782
Rafael Feliciano <rafaelfelic@gmail.com>
2025-02-12 22:27:52 +1000

    Vercel analytics implemented

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 import dynamic from "next/dynamic";
+import { Analytics } from "@vercel/analytics/react";
 
 const AnimatedBackground = dynamic(
   () => import("../components/AnimatedBackground"),
@@ -14,6 +15,7 @@ function MyApp({ Component, pageProps }) {
     <div className="relative min-h-screen bg-[url('/images/background.webp')] bg-center bg-cover bg-no-repeat">
       <div className="absolute inset-0 bg-black opacity-60"></div>
       <AnimatedBackground />
+      <Analytics />
       <Layout>
         <Component {...pageProps} />
       </Layout>
